7ca72644fec0bc10fd0e29472f74d67ba0d29133
limid404 <85405089+limid404@users.noreply.github.com>
2021-07-16 22:06:11 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -23,7 +23,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/limid404//bot-jstlimid/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/limid404/bot-jstlimid/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
